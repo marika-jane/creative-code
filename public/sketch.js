1ff9e58c0980c3a3e3ae111d32d29003b3a8edb7
Marika Fiolitakis <marika.jf@gmail.com>
2020-10-16 10:56:00 +1300

    add lerp

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,22 @@
+let x = 0
+let target = 300
+
 function setup () {
-  createCanvas(200, 200)
+  createCanvas(600, 600)
 }
 
-function draw() {
-    background(51)
+function mousePressed () {
+  target = mouseX
+}
+
+function draw () {
+  x = lerp(x, target, 0.05)
+
+  background(180, 50, 90)
+
+  fill(0, 0, 175, 150)
+  ellipse(target, 200, 64, 64)
+
+  fill(175, 0, 255, 150)
+  ellipse(x, 200, 64, 64)
 }
